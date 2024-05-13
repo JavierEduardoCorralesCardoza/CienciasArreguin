dddaf95432f692140267d1ead8aca029f8ecee6f
JavierEduardoCorralesCardoza <javier.e.corrales.cardoza@gmail.com>
2024-05-12 21:26:08 -0600

    First commit

---
+++ arreguin/src/main/java/com/ciencias_arreguin/arreguin/ArreguinApplication.java
@@ -0,0 +1,13 @@
+package com.ciencias_arreguin.arreguin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArreguinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArreguinApplication.class, args);
+	}
+
+}
---
+++ arreguin/src/test/java/com/ciencias_arreguin/arreguin/ArreguinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciencias_arreguin.arreguin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArreguinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
